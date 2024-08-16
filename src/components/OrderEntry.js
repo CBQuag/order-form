5c1a9597e3e7f00600503512b228794ed7576721
Conrad Quagliaroli <conradbquag@gmail.com>
2024-08-16 14:25:02 -0400

    cleaned up orderid generation

--- src/components/OrderEntry.js
+++ src/components/OrderEntry.js
@@ -7,19 +7,23 @@ const OrderEntry = () => {
     const { setOrders, orders, orderData } = useContext(OrderContext);
     const [item, setItem] = useState('');
     const [category, setCategory] = useState('');
-    const [quantity, setQuantity] = useState(0);
-    const [price, setPrice] = useState(0);
+    const [quantity, setQuantity] = useState('');
+    const [price, setPrice] = useState('');
     
     //on click, adds new order object into orders
     const handleSubmit = () => {
         const newOrder={
             item: item,
-            order_id: orders.length,
+            order_id: orders[orders.length-1].order_id+1,
             date: Date.now(),
             category: category,
             quantity: quantity,
             price: price
         };
+        setItem('');
+        setCategory('');
+        setQuantity('');
+        setPrice('');
         setOrders((prev)=>[...prev, newOrder])
     }
     
@@ -27,19 +31,19 @@ const OrderEntry = () => {
         <ul className="order-form" >
             <li>
                 <label htmlFor="item">Item Name: </label>
-                <input type="text" name="" id="item" onInput={e=>setItem(e.target.value)}/>  
+                <input type="text" name="" id="item" value={item} onInput={e=>setItem(e.target.value)}/>  
             </li>
             <li>
                 <label htmlFor="category">Category: </label>
-                <input type="text" name="" id="category" onInput={e=>setCategory(e.target.value)}/>  
+                <input type="text" name="" id="category" value={category} onInput={e=>setCategory(e.target.value)}/>  
             </li>
             <li>
                 <label htmlFor="item">Quantity: </label>
-                <input type="number" name="" id="quantity" onInput={e=>setQuantity(e.target.value)}/>  
+                <input type="number" name="" id="quantity" value={quantity} onInput={e=>setQuantity(e.target.value)}/>  
             </li>
             <li>
                 <label htmlFor="item">Price: </label>
-                <input type="number" name="" id="price" onInput={e=>setPrice(e.target.value)}/>  
+                <input type="number" name="" id="price" value={price} onInput={e=>setPrice(e.target.value)}/>  
             </li>
             <button type="button" className="subBut" onClick={()=>handleSubmit()}>Submit</button>
         </ul>
