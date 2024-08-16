dc4b8f6d9da727082c31c03ba83affb7ff3d1bfb
Conrad Quagliaroli <conradbquag@gmail.com>
2024-08-16 14:53:47 -0400

    made logo placeholder into a reset button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import './Header.css'
+import { useContext} from "react";
+import OrderContext from "../context/OrderContext";
+import order_data from "../context/order-data";
+
 
 const Header = () => {
+    
+    const {shorten, setOrders, orders}=useContext(OrderContext)
+    const handleClick = () => {
+        setOrders(order_data)
+    }
+    
     return (
     <div className="header">    
-            <div>Logo</div>
+            <div onClick={()=>handleClick()}>Logo</div>
             <div className="menu">
                 <NavLink    to=""
                             className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}>
